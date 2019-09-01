f7244f1be6e431e3d26cd69b9395dd65c728e40d
quyctd <quy.dc98@gmail.com>
2019-09-01 15:06:14 +0700

    Added user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'quyctd', password: '123')
+User.create(username: 'datnt', password: '123')
+User.create(username: 'hailtm', password: '123')
