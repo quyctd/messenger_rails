8a7e47eda173cef2622f850ebf85457ec1f320bb
quyctd <quy.dc98@gmail.com>
2019-09-01 18:38:06 +0700

    Authenticate user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @account = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username].downcase)
+    if @user&.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:negative] = 'Username or password not correct'
+      render 'new'
+    end
+  end
 end
