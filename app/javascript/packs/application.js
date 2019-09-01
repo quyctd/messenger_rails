f1dedd9b723db022dac42266683b771949517d59
quyctd <quy.dc98@gmail.com>
2019-09-01 11:00:35 +0700

    Update gem file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-
+require('jquery');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +17,10 @@ require("channels");
 // const imagePath = (name) => images(name, true)
 //= require jquery
 //= require semantic-ui
+
+window.jQuery = $;
+window.$ = $;
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+});
